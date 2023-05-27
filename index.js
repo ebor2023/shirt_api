f1be0372e90eaea9970f020a9935ac65bf9f27db
ebor2023 <134803017+ebor2023@users.noreply.github.com>
2023-05-27 20:26:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+  
+const express  = require('express'); // import express package
+const app = express(); 
+ const PORT = 8080; 
+
+ 
+app.use(express.json()) 
+
+
+ app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕 ', 
+        size: 'large'
+    })
+ });
+
+ app.post('/tshirt/:id', (req, res) => {
+
+        const { id } = req.params; 
+        const { logo } = req.body; 
+
+        if (!logo) {
+            res.status(418).send({
+                message: 'We need a logo!'
+            })
+        } 
+        res.send({
+            tshirt:` with your ${logo} and ID of ${id}`
+        }); 
+ })
+
+
+ 
+ 
+ 
+ 
+ app.listen(
+    PORT, 
+    () => console.log(`it's alive on http://localhost:${PORT}`) 
+
+ ) 
